7e749039fc0472e657c5e397a160706e76fb4bdb
Lasse Bærland Strand <lassebs@stud.ntnu.no>
2024-09-09 13:13:40 +0200

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+/// <reference types="vitest" />
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    open: true,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: 'src/test/setup.ts',
+  }
+});
