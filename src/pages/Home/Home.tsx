1c9a184dd94ced3968a70f989a0aea42831ba620
Lasse Bærland Strand <lassebs@stud.ntnu.no>
2024-09-10 14:13:24 +0200

    add: Add example component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,11 @@
+import Button from '../../components/Button/Button';
+
 const Home = () => {
   return (
     <div>
       <h1>43 °C</h1>
       <h1>🔥😰🔥</h1>
+      <Button text="Press me to do absolutely nothing" />
     </div>
   );
 };
