a195cbed2b9b2565263eed783975b9f6324cc04a
Randy <kriosen03@outlook.com>
2024-09-12 15:01:44 +0200

    add: Add support for Locations

---
+++ src/utils/locations.ts
@@ -0,0 +1,10 @@
+import { Location } from '../types/api-types';
+
+// TODO: Use real locations and correct coordinates
+const LOCATIONS: Location[] = [
+  {city_name: 'New York', country_name: 'USA', latitude: 40.7128, longitude: -74.006},
+  {city_name: 'London', country_name: 'UK', latitude: 51.5074, longitude: -0.1278},
+  {city_name: 'Tokyo', country_name: 'Japan', latitude: 35.6895, longitude: 139.6917},
+];
+
+export default LOCATIONS;
