75e115bf7e71ceff3222390742d8d10c1b30e36b
Randy <kriosen03@outlook.com>
2024-09-12 15:01:01 +0200

    update: Use new LOCATIONS constant

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,34 +1,38 @@
 import {IconArrowLeft, IconArrowRight} from '@tabler/icons-react';
-import {Location} from '../../types/api-types';
+import {useParams} from 'react-router-dom';
+import LOCATIONS from '../../utils/locations';
 import WeatherCard from '../WeatherCard/WeatherCard';
 import styles from './Carousel.module.css';
 
-const getPreviousCity = (currentIndex: number, locations: Location[]) => {
+const getPreviousCity = (currentIndex: number) => {
   if (currentIndex === 0) {
-    return locations[locations.length - 1];
+    return LOCATIONS[LOCATIONS.length - 1];
   }
-  return locations[currentIndex - 1];
+  return LOCATIONS[currentIndex - 1];
 };
 
-const getNextCity = (currentIndex: number, locations: Location[]) => {
-  if (currentIndex === locations.length - 1) {
-    return locations[0];
+const getNextCity = (currentIndex: number) => {
+  if (currentIndex === LOCATIONS.length - 1) {
+    return LOCATIONS[0];
   }
-  return locations[currentIndex + 1];
+  return LOCATIONS[currentIndex + 1];
 };
 
-const Carousel = (locations: Location[], currentIndex: number) => {
+const Carousel = () => {
+  const {locationName} = useParams();
+  const currentIndex = LOCATIONS.findIndex((location) => location.city_name === locationName);
+
   return (
     <>
-      <WeatherCard {...locations[currentIndex]} />
+      <WeatherCard {...LOCATIONS[currentIndex]} />
       <div className={styles.buttonContainer}>
-        <a className={styles.a} href={'/' + getPreviousCity(currentIndex, locations).city_name}>
+        <a className={styles.a} href={'/location/' + getPreviousCity(currentIndex).city_name}>
           <IconArrowLeft />
-          {getPreviousCity(currentIndex, locations).city_name}
+          {getPreviousCity(currentIndex).city_name}
         </a>
-        <a className={styles.a} href={'/' + getNextCity(currentIndex, locations).city_name}>
+        <a className={styles.a} href={'/location/' + getNextCity(currentIndex).city_name}>
           <IconArrowRight />
-          {getNextCity(currentIndex, locations).city_name}
+          {getNextCity(currentIndex).city_name}
         </a>
       </div>
     </>
