fa54fed2bdc52932c65ee2ff6a783f339d82c32d
Randy <kriosen03@outlook.com>
2024-09-12 15:02:14 +0200

    add: Add a location route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home/Home';
+import Location from './pages/Location/Location';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/location/:locationName" element={<Location />} />
         {/* <Route path="/about" element={<About />} /> */}
       </Routes>
     </BrowserRouter>
