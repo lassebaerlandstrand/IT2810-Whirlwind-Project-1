8fdbbe4d21e91bfc18cb84b59270071c4cb434ff
Lasse Bærland Strand <lassebaerlandstrand@gmail.com>
2024-09-13 09:34:19 +0200

    add: Add Simple dropdown menu

---
+++ src/components/SortDropDown/SortDropDown.tsx
@@ -0,0 +1,44 @@
+import {IconChevronDown} from '@tabler/icons-react';
+import {useState} from 'react';
+import {Location} from '../../types/api-types';
+import styles from './SortDropDown.module.css';
+
+const options = {
+  Name: (list: Location[]) => list.sort((a, b) => a.city_name.localeCompare(b.city_name)),
+};
+
+type DropDownContentProps = {
+  open: boolean;
+};
+
+const DropDownContent = ({open}: DropDownContentProps) => {
+  return <p>Hello World</p>;
+};
+
+type DropDownButtonProps = {
+  open: boolean;
+  toggleOpen: () => void;
+  selectedOption: string;
+};
+
+const DropDownButton = ({open, selectedOption}: DropDownButtonProps) => {
+  return (
+    <button className={styles.dropDownButton}>
+      {selectedOption} <span className={styles.iconSpan}>{<IconChevronDown />}</span>
+    </button>
+  );
+};
+
+const SortDropDown = () => {
+  const [open, setOpen] = useState<boolean>(false);
+  const toggleOpen = () => setOpen(!open);
+
+  return (
+    <menu className={styles.container}>
+      <DropDownButton open={open} toggleOpen={toggleOpen} selectedOption="Name" />
+      <DropDownContent open={open} />
+    </menu>
+  );
+};
+
+export default SortDropDown;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,30 @@
+import {useState} from 'react';
 import Button from '../../components/Button/Button';
+import SortDropDown from '../../components/SortDropDown/SortDropDown';
 
 const Home = () => {
+  const [cities] = useState<{name: string}[]>([
+    {name: 'Trondheim'},
+    {name: 'Oslo'},
+    {name: 'Stavanger'},
+    {name: 'Ålesund'},
+    {name: 'Gjøvik'},
+    {name: 'Tønsberg'},
+    {name: 'Osen'},
+  ]); // Dummy data, to be changed
+  const [filteredCities, setFilteredCities] = useState(cities);
+
+  const handleSort = (sortCondition: (a: {name: string}, b: {name: string}) => number) => {
+    const sortedData = [...filteredCities].sort(sortCondition);
+    setFilteredCities(sortedData);
+  };
+
   return (
     <div>
       <h1>43 °C</h1>
       <h1>🔥😰🔥</h1>
       <Button text="Press me to do absolutely nothing" />
+      <SortDropDown />
     </div>
   );
 };
