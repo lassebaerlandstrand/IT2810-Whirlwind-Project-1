01bf9e129f59985545a756e569cbda322c4e5444
Randy <kriosen03@outlook.com>
2024-09-15 13:55:47 +0200

    update: Rename function to more descriptive name

--- src/components/WeatherCard/WeatherCard.tsx
+++ src/components/WeatherCard/WeatherCard.tsx
@@ -3,7 +3,7 @@ import { useWeather } from '../../hooks/useWeather';
 import { Location, WeatherInfo } from '../../types/api-types';
 import styles from './WeatherCard.module.css';
 
-const getWeatherEmoji = (data: WeatherInfo) => {
+const determineWeather = (data: WeatherInfo) => {
   if (data.precipitation_amount > 0) {
     return [<IconCloudRain />, 'Rainy'];
   }
@@ -17,12 +17,12 @@ const WeatherCard = (location: Location) => {
   const { data } = useWeather(location);
   return (
     <div className={styles.card}>
-      {data ? getWeatherEmoji(data)[0] : <IconRotateClockwise className={styles.rotating} />}
+      {data ? determineWeather(data)[0] : <IconRotateClockwise className={styles.rotating} />}
       <p className={styles.city}>
         {location.city_name}, {location.country_name}
       </p>
       <p className={styles.temperature}>{data ? data.air_temperature : '---'}°C</p>
-      <p>{data ? getWeatherEmoji(data)[1] : 'Loading...'}</p>
+      <p>{data ? determineWeather(data)[1] : 'Loading...'}</p>
     </div>
   );
 };
