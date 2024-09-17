f69fa8d04c4c84b1f6aa165fea23213cc0d65991
Randy <kriosen03@outlook.com>
2024-09-17 20:36:52 +0200

    update: use <link> component instead of <a> tag

--- src/components/HomeButton/HomeButton.tsx
+++ src/components/HomeButton/HomeButton.tsx
@@ -1,12 +1,13 @@
 import { IconList } from '@tabler/icons-react';
+import { Link } from 'react-router-dom';
 import styles from './HomeButton.module.css';
 
 const HomeButton = () => {
   return (
-    <a className={styles.a} href="/">
+    <Link className={styles.a} to="/">
       <IconList className={styles.icon} />
       Go back to list
-    </a>
+    </Link>
   );
 };
 
