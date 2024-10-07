35824617a781e63f1d595914eed1d2a5311dcd37
Adam S <134907338+adam-sioud@users.noreply.github.com>
2024-10-07 23:47:50 +0200

    fix: adjusted code according to lassebs feedback
    fix: adjusted code according to lassebs feedback

--- src/components/WeatherList/WeatherList.tsx
+++ src/components/WeatherList/WeatherList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useFavorites } from '../../contexts/FavoritesFunctions';
 import { Location } from '../../types/api-types';
 import ListWeatherCard from '../ListWeatherCard/ListWeatherCard';
@@ -10,11 +10,6 @@ interface WeatherListProps {
 
 const WeatherList: React.FC<WeatherListProps> = ({ locations }) => {
   const { favorites } = useFavorites();
-  const [hasFavorites, setHasFavorites] = useState(false);
-
-  useEffect(() => {
-    setHasFavorites(favorites.length > 0);
-  }, [favorites]);
 
   const favoritedLocations = locations.filter((location) =>
     favorites.some((fav) => fav.city_name === location.city_name && fav.country_name === location.country_name),
@@ -41,7 +36,7 @@ const WeatherList: React.FC<WeatherListProps> = ({ locations }) => {
       )}
       {nonFavoritedLocations.length > 0 && (
         <section className={styles.section}>
-          <h2>{hasFavorites ? 'Other Locations' : 'Locations'}</h2>
+          <h2>{favorites.length > 0 ? 'Other Locations' : 'Locations'}</h2>
           <ul className={styles.list}>
             {nonFavoritedLocations.map((location, index) => (
               <li key={`nonfav-${index}`}>
